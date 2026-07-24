24a4d65c43c2bd975c9f6dc669e0e5f0cebf6e37
HolyPrasetya <prasetya.holy005@gmail.com>
2026-07-24 13:34:47 +0700

    Initial Commit

---
+++ DSSHHH/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DSSHHH
+//
+//  Created by Ignasius Holy Prasetya on 24/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DSSHHH/DSSHHHApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSSHHHApp.swift
+//  DSSHHH
+//
+//  Created by Ignasius Holy Prasetya on 24/07/26.
+//
+
+import SwiftUI
+
+@main
+struct DSSHHHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
